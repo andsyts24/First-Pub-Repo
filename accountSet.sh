1d9b4445cb4f75ee152d50935d8337f6080a31fd
andsyts24 <andsyts24@gmail.com>
2025-04-29 10:24:37 -0500

    Create accountSet.sh
    account setup script

---
+++ accountSet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ "$EUID" = 0 ]]; then
+  echo "(1) You are root... Running rest of script..."
+else
+  echo "(2) You must be root to run this script..."
+fi
+
+password=$( shuf -n /usr/share/dict/words )
+echo "Setting up accounts..."
+echo $USER:$password | chpasswd
+rpassword=$( shuf -n 1 /usr/share/dict/words )
+echo root:$rpassword | chpasswd
+echo "Done with setup..."
+sleep 4
+echo "Rebooting System Now..."
+reboot
